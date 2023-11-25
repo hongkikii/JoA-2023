2d1807ba4ee83063c3e22d81e9897ee8ba24b220
hongkikii <hyangmeeh@icloud.com>
2023-11-25 20:18:18 +0900

    fix: 중복 차단 요청 막기

--- src/main/java/com/mjuAppSW/joA/geography/block/BlockRepository.java
+++ src/main/java/com/mjuAppSW/joA/geography/block/BlockRepository.java
@@ -1,6 +1,11 @@
 package com.mjuAppSW.joA.geography.block;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BlockRepository extends JpaRepository<Block, Long> {
+
+    @Query("SELECT b FROM Block b WHERE b.blocker = :blockerId AND b.blocked = :blockedId")
+    Block findEqualBlock(@Param("blockerId") Long blockerId, @Param("blockedId") Long blockedId);
 }
