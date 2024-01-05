a824feecb94d921aaf67903de595c439d4de17c4
hongkikii <hyangmeeh@icloud.com>
2024-01-06 03:16:39 +0900

    refactor: Vote API 표준화

--- src/main/java/com/mjuAppSW/joA/domain/vote/dto/response/VoteOwnerResponse.java
+++ src/main/java/com/mjuAppSW/joA/domain/vote/dto/response/VoteOwnerResponse.java
@@ -1,22 +1,24 @@
 package com.mjuAppSW.joA.domain.vote.dto.response;
 
+import com.mjuAppSW.joA.domain.member.Member;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
 @Schema(description = "투표 화면 사용자 정보 Response")
+@Builder(access = AccessLevel.PRIVATE)
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class VoteOwnerResponse {
-    private Integer status;
-    private String name;
-    private String urlCode;
+    private final String name;
+    private final String urlCode;
 
-    public VoteOwnerResponse(Integer status) {
-        this.status = status;
-    }
-
-    public VoteOwnerResponse(Integer status, String name, String urlCode) {
-        this.status = status;
-        this.name = name;
-        this.urlCode = urlCode;
+    public static VoteOwnerResponse of(Member member) {
+        return VoteOwnerResponse.builder()
+                .name(member.getName())
+                .urlCode(member.getUrlCode())
+                .build();
     }
 }
