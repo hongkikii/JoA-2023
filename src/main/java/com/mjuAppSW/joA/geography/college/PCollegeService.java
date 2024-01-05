d4674e1b2988da666001a11a7c55df8e62530327
hongkikii <hyangmeeh@icloud.com>
2024-01-06 04:04:25 +0900

    refactor: 학교 범위 등록 API 표준화

--- src/main/java/com/mjuAppSW/joA/geography/college/PCollegeService.java
+++ src/main/java/com/mjuAppSW/joA/geography/college/PCollegeService.java
@@ -1,2 +1,36 @@
-package com.mjuAppSW.joA.geography.college;public class PCollegeService {
+package com.mjuAppSW.joA.geography.college;
+
+import com.mjuAppSW.joA.geography.location.dto.request.PolygonRequest;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Polygon;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PCollegeService {
+
+    private final PCollegeRepository pCollegeRepository;
+
+    @Transactional
+    public void setPolygon(PolygonRequest request) {
+        Polygon polygon = makePolygon(request);
+        PCollege college = new PCollege(request.getCollegeId(), polygon);
+        pCollegeRepository.save(college);
+    }
+
+    private Polygon makePolygon(PolygonRequest request) {
+        GeometryFactory geometryFactory = new GeometryFactory();
+
+        Coordinate[] coordinates = new Coordinate[] {
+                new Coordinate(request.getTopLeftLng(), request.getTopLeftLat()),
+                new Coordinate(request.getTopRightLng(), request.getTopRightLat()),
+                new Coordinate(request.getBottomRightLng(), request.getBottomRightLat()),
+                new Coordinate(request.getBottomLeftLng(), request.getBottomLeftLat()),
+                new Coordinate(request.getTopLeftLng(), request.getTopLeftLat())
+        };
+        return geometryFactory.createPolygon(coordinates);
+    }
 }
