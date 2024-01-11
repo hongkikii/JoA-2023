373d8f48b9d0e82c5170c2c0cbbb260fb74e45eb
hongkikii <hyangmeeh@icloud.com>
2024-01-11 17:43:45 +0900

    refactor: Heart API 리팩토링

--- src/main/java/com/mjuAppSW/joA/domain/heart/exception/RoomAlreadyExistedException.java
+++ src/main/java/com/mjuAppSW/joA/domain/heart/exception/RoomAlreadyExistedException.java
@@ -3,9 +3,9 @@ package com.mjuAppSW.joA.domain.heart.exception;
 import com.mjuAppSW.joA.common.exception.BusinessException;
 import com.mjuAppSW.joA.common.exception.ErrorCode;
 
-public class RoomExistedException extends BusinessException {
+public class RoomAlreadyExistedException extends BusinessException {
 
-    public RoomExistedException() {
+    public RoomAlreadyExistedException() {
         super(ErrorCode.ROOM_EXISTED);
     }
 }
